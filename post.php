908e41a0fe33639535cb36279e386b16dd470591
Yuki <kaiyakutarou@gmail.com>
2025-05-27 13:17:40 +0900

    Update post.php

--- post.php
+++ post.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit;
 }
 
-$content = isset($_POST['content']) ? trim($_POST['content']) : '';
+$content = isset($_POST['message']) ? trim($_POST['message']) : '';
 
 // バリデーション
 if ($content === '') {
@@ -22,6 +22,7 @@ if (mb_strlen($content) > 200) {
 // 新しい投稿データの作成
 $newPost = [
     'id' => uniqid(),
+    'name' => isset($_POST['name']) ? htmlspecialchars(trim($_POST['name']), ENT_QUOTES, 'UTF-8') : '匿名',
     'content' => htmlspecialchars($content, ENT_QUOTES, 'UTF-8'),
     'likes' => 0,
     'timestamp' => date('Y-m-d H:i:s')
@@ -29,9 +30,9 @@ $newPost = [
 
 // JSONファイルに保存
 $file = 'data/posts.json';
-$posts = loadPosts($file);
+$posts = load_posts($file);
 $posts[] = $newPost;
-savePosts($file, $posts);
+save_json_file($file, $posts);
 
 // 投稿完了後、トップページにリダイレクト
 header('Location: index.php');
